7ef022d6f59f87c22633d90325d4b8aa4e79a724
motohiroe shiobara <siobara.motohiro@gmail.com>
2017-12-02 12:31:40 +0900

    表示

--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,78 @@
+// import React, { Component } from 'react'
+// import { render } from 'react-dom'
+
+// class App extends Component {
+//   render() {
+//     const { todoLists } = this.props
+//     return (
+//         <div>
+//           <ul>
+//             {this.listCreator(todoLists)}
+//           </ul>
+//         <div>
+//     )
+//   }
+
+//   listCreator(todoLists) {
+//     let lists = []
+
+//     todoLists.forEach((todo, i, todoLists) => {
+//       let idName = `todo_input_${i}`
+
+//       lists.push(
+//         <li key={i}>
+//           <input key={i} type='checkbox' value={todo['title']} id={idName} />
+//           <label key={i + 1} htmlFor={idName}>
+//             {todo['title']}
+//           </label>
+//         </li>
+//       )
+//     })
+
+//     return lists
+//   }
+// }
+
+// render(
+//   <App todoLists={todoLists: [{title: "title"}]} />,
+//   document.getElementById('container'))
+
+
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 class App extends Component {
   render() {
-    const { title } = this.props
+    const { todoLists } = this.props
     return (
       <div>
-        <h1>
-          {title}
-        </h1>
+        <ul>
+        {this.listCreator(todoLists)}
+        </ul>
       </div>
     )
+  }
+
+  listCreator(todoLists) {
+    let lists = []
+
+    todoLists.forEach((todo, i, todoLists) => {
+      let idName = `todo_input_${i}`
+
+      lists.push(
+        <li key={i}>
+          <input key={i} type='checkbox' value={todo['title']} id={idName} />
+          <label key={i + 1} htmlFor={idName}>
+            {todo['title']}
+          </label>
+        </li>
+      )
+    })
+
+    return lists
   }
 }
 
 render(
-  <App title="Hello React" />,
-  document.getElementById('container'))
+    <App todoLists={[{title: "テスト"}, {title: "テスト2"}]} />,
+    document.getElementById('container'))
