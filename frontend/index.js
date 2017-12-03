811c9f7c173cc1b6f39019ca08cd2522c3bad51f
motohiroe shiobara <siobara.motohiro@gmail.com>
2017-12-03 10:54:35 +0900

    todoリストの追加機能の実装

--- frontend/index.js
+++ frontend/index.js
@@ -1,54 +1,25 @@
-// import React, { Component } from 'react'
-// import { render } from 'react-dom'
-
-// class App extends Component {
-//   render() {
-//     const { todoLists } = this.props
-//     return (
-//         <div>
-//           <ul>
-//             {this.listCreator(todoLists)}
-//           </ul>
-//         <div>
-//     )
-//   }
-
-//   listCreator(todoLists) {
-//     let lists = []
-
-//     todoLists.forEach((todo, i, todoLists) => {
-//       let idName = `todo_input_${i}`
-
-//       lists.push(
-//         <li key={i}>
-//           <input key={i} type='checkbox' value={todo['title']} id={idName} />
-//           <label key={i + 1} htmlFor={idName}>
-//             {todo['title']}
-//           </label>
-//         </li>
-//       )
-//     })
-
-//     return lists
-//   }
-// }
-
-// render(
-//   <App todoLists={todoLists: [{title: "title"}]} />,
-//   document.getElementById('container'))
-
-
 import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { todoLists: props.todoLists, value: '' }
+  }
+
   render() {
-    const { todoLists } = this.props
     return (
       <div>
         <ul>
-        {this.listCreator(todoLists)}
+          {this.listCreator(this.state.todoLists)}
         </ul>
+        <div>
+          <h3>TODOリストに追加する</h3>
+          <form onSubmit={(e) => this.handleSubmit(e)}>
+          <input type='text' value={this.state.value} onChange={(e) => this.handleChange(e)} />
+          <input type="submit" value="追加" />
+          </form>
+        </div>
       </div>
     )
   }
@@ -70,6 +41,25 @@ class App extends Component {
     })
 
     return lists
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+
+    let title = this.state.value
+
+    let todoLists = [
+      ...this.state.todoLists,
+      {
+        title: title
+      }
+    ]
+
+    this.setState({ todoLists: todoLists, value: '' })
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
   }
 }
 
